309d683a9db2179b9b474e98206fd3b935beb0a0
Bernd Malle <bernd.malle@gmail.com>
2015-11-23 03:34:13 +0100

    verified publishing via npm works, had to remove 'npm install' from the prepublish target in order to avoid infinite loop

--- dist/core/Nodes.js
+++
@@ -1,8 +0,0 @@
-var BaseNode = (function () {
-    function BaseNode(_id, _label) {
-        this._id = _id;
-        this._label = _label;
-    }
-    return BaseNode;
-})();
-exports.BaseNode = BaseNode;
