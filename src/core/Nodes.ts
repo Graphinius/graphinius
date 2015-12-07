97233e39361b3b274242de134b973a6c4ab1a640
Bernd Malle <bernd.malle@gmail.com>
2015-12-07 11:57:35 +0100

    defined handling of adding edge cases

--- src/core/Nodes.ts
+++ src/core/Nodes.ts
@@ -1,6 +1,13 @@
 import * as Edges from "./Edges";
+var _ = require('underscore');
 
-
+enum EdgeType {
+	IN,
+	OUT,
+	DIRECTED,
+	UNDIRECTED,
+	ALL
+}
 
 interface IBaseNode {
 	// Public properties
@@ -11,15 +18,16 @@ interface IBaseNode {
 	getUntypedFeatures() : Array<{ [k:string] : any }>;
 	
 	// Degrees
-	degree(type: DegreeType) : number;
+	degree(type: EdgeType) : number;
 	
 	// EDGE methods
-	// maybe simplify by internal type
-	// getEdges() 		: Array<Edges.IBaseEdge>;
-	// getInEdges() 	: Array<Edges.IBaseEdge>;
-	// getOutEdges() 	: Array<Edges.IBaseEdge>;
-	// getDirEdges() 	: Array<Edges.IBaseEdge>;
-	// getUndEdges() 	: Array<Edges.IBaseEdge>;
+	// maybe simplify by internal type => YEP
+	addEdge(edge: Edges.IBaseEdge) : void;
+	
+	// getEdges(type: EdgeType) : Array<Edges.IBaseEdge>;
+	// isEdgeType(id: number) : EdgeType;
+	// hasEdge(id: number) : boolean;
+	// removeEdge(id: number) : Edges.IBaseEdge;
 	
 	// untyped FEATURE methods
 	// getFeature
@@ -29,24 +37,20 @@ interface NodeConstructorOptions {
 	
 }
 
-enum DegreeType {
-	IN,
-	OUT,
-	DIRECTED,
-	UNDIRECTED,
-	ALL
-}
-
 
 class BaseNode implements IBaseNode {
 	protected _edges	: Array<Edges.IBaseEdge>;
 	protected _untyped_features	: Array<{ [k:string] : any }>;
 	
-	// computing degree on the fly via edge array?
-	// or better internally update different degree
-	// measures upon edge addition / removal?
-	// For now, let's decide on the complicated version
-	// that is faster to query though...
+	/**
+	 * computing degree on the fly via edge array?
+	 * or better internally update different degree
+	 * measures upon edge addition / removal?
+	 * The latter suggestion is better in the sense of
+	 * DRY / reduced complexity
+	 * For now, let's decide on the complicated version
+	 * that is faster to query though...
+	 */
 	protected _in_degree;
 	protected _out_degree;
 	protected _dir_degree;
@@ -69,22 +73,45 @@ class BaseNode implements IBaseNode {
 		return this._untyped_features;
 	}
 	
-	degree(type: DegreeType) : number {
+	degree(type: EdgeType) : number {
 		switch(type) {
-			case DegreeType.IN:
+			case EdgeType.IN:
 				return this._in_degree;
-			case DegreeType.OUT:
+			case EdgeType.OUT:
 				return this._out_degree;
-			case DegreeType.DIRECTED:
+			case EdgeType.DIRECTED:
 				return this._dir_degree;
-			case DegreeType.UNDIRECTED:
+			case EdgeType.UNDIRECTED:
 				return this._und_degree;
-			case DegreeType.ALL:
+			case EdgeType.ALL:
 				return this._all_degree;
+		}
+	}
+	
+	/**
+	 * We have to: 
+	 * 1. throw an error if the edge is already attached
+	 * 2. add it to the edge array
+	 * 3. check type of edge (directed / undirected)
+	 * 4. update our degrees accordingly
+	 * This is a design decision we should defend
+	 */
+	addEdge(edge: Edges.IBaseEdge) : void {
+		if ( _.contains( this._edges, edge ) ) {
+			throw new Error("Cannot attach same edge multiple times.");
+		}
+		
+		this._edges.push(edge);		
+		var dir = edge.isDirected();		
+		if ( dir ) {
+			
+		} else {			
+			this._und_degree++;
+			this._all_degree++;
 		}
 	}
 	
 }
 
 
-export { DegreeType, IBaseNode, BaseNode };
+export { EdgeType, IBaseNode, BaseNode };
--- test/core/NodeTests.ts
+++ test/core/NodeTests.ts
@@ -6,7 +6,7 @@ import * as $E from '../../src/core/Edges';
 
 var expect = chai.expect;
 var Edge = $E.BaseEdge;
-var Degree = $N.DegreeType;
+var Degree = $N.EdgeType;
 
 
 
@@ -26,6 +26,7 @@ describe('==== NODE TESTS ====', () => {
 		});
 	});
 	
+	
 	describe('Node optional instantiation parameter tests', () => {
 		it('should correctly set default untyped features to an empty array', () => {
 			var node = new $N.BaseNode(id, label);
@@ -42,6 +43,7 @@ describe('==== NODE TESTS ====', () => {
 		});
 	});
 	
+	
 	describe('Node default degrees', () => {
 		it('should automatically report all degree values as zero upon instantiations', () => {
 			var node = new $N.BaseNode(id, label);
@@ -51,6 +53,77 @@ describe('==== NODE TESTS ====', () => {
 			expect(node.degree(Degree.UNDIRECTED)).to.equal(0);
 			expect(node.degree(Degree.ALL)).to.equal(0);
 		});
+	});
+	
+	
+	describe('Node edge addition / removal / degree tests', () => {
+		var node_a = new $N.BaseNode(id, label),
+				node_b = new $N.BaseNode(id, label),
+				e_id = 55,
+				e_label = "Edgy";
+		
+		
+		describe('Node edge addition tests', () => {
+		
+		/**
+			* A LOT has is to be expected here...
+			* 1. an edge should have been added
+			* 2. in degree should still be 0
+			* 3. out degree should still be 0
+			* 4. dir degree should still be 0
+			* 5. und degree should be 1
+			* 6. all degree should be 1
+			* We only test on ONE node per case,
+			* as the graph class will encapsulte
+			* adding edges to both nodes later...
+			*/
+			it('Should correctly add an undirected edge', () => {
+				var und_edge = new $E.BaseEdge(e_id, e_label, node_a, node_b, {
+					directed: false,
+					weighted: false
+				});
+				var in_deg_a 	= node_a.degree(Degree.IN),
+						out_deg_a = node_a.degree(Degree.OUT),
+						dir_deg_a = node_a.degree(Degree.DIRECTED),
+						und_deg_a = node_a.degree(Degree.UNDIRECTED),
+						all_deg_a = node_a.degree(Degree.ALL);
+						
+				node_a.addEdge(und_edge);
+	
+				expect(node_a.degree(Degree.IN)).to.equal(in_deg_a);
+				expect(node_a.degree(Degree.OUT)).to.equal(out_deg_a);
+				expect(node_a.degree(Degree.DIRECTED)).to.equal(dir_deg_a);
+				expect(node_a.degree(Degree.UNDIRECTED)).to.equal(und_deg_a + 1);
+				expect(node_a.degree(Degree.ALL)).to.equal(all_deg_a + 1);
+			});
+			
+			
+			it('Should throw an error if we try to add an edge more than once', () => {
+				var und_edge = new $E.BaseEdge(e_id, e_label, node_a, node_b, {
+					directed: false,
+					weighted: false
+				});
+						
+				node_a.addEdge(und_edge);
+				
+				// degrees after first edge addition
+				var in_deg_a 	= node_a.degree(Degree.IN),
+						out_deg_a = node_a.degree(Degree.OUT),
+						dir_deg_a = node_a.degree(Degree.DIRECTED),
+						und_deg_a = node_a.degree(Degree.UNDIRECTED),
+						all_deg_a = node_a.degree(Degree.ALL);
+						
+				// here we should get an exception for duplicate edges
+				expect(node_a.addEdge.bind(node_a, und_edge)).to.throw("Cannot attach same edge multiple times.");
+				expect(node_a.degree(Degree.IN)).to.equal(in_deg_a);
+				expect(node_a.degree(Degree.OUT)).to.equal(out_deg_a);
+				expect(node_a.degree(Degree.DIRECTED)).to.equal(dir_deg_a);
+				expect(node_a.degree(Degree.UNDIRECTED)).to.equal(und_deg_a);
+				expect(node_a.degree(Degree.ALL)).to.equal(all_deg_a);
+			});
+			
+		});
+		
 	});
 
 });
