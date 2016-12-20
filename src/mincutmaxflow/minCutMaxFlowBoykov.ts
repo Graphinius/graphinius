a04a80712d489a3897f1d1be2697ddabb01e58f8
Nicola Giuliani <giulianico6@gmail.com>
2016-12-20 20:16:29 +0100

    added 'grow' part of the mincut algorithm

--- src/mincutmaxflow/minCutMaxFlowBoykov.ts
+++ src/mincutmaxflow/minCutMaxFlowBoykov.ts
@@ -29,13 +29,14 @@ export interface MCMFState {
   orphans     : {[key:string] : $N.IBaseNode};
   treeS       : {[key:string] : $N.IBaseNode};
   treeT       : {[key:string] : $N.IBaseNode};
+	parents			: {[key:string] : $N.IBaseNode};
   path        : Array<$N.IBaseNode>
 }
 
 
 
 /**
- * 
+ *
  */
 class MCMFBoykov implements IMCMFBoykov {
 
@@ -45,16 +46,17 @@ class MCMFBoykov implements IMCMFBoykov {
     orphans     : {},
     treeS       : {},
     treeT       : {},
+		parents			: {},
     path        : []
   };
-  
-  constructor( private _graph 	 : $G.IGraph, 
+
+  constructor( private _graph 	 : $G.IGraph,
 						   private _source	 : $N.IBaseNode,
                private _sink     : $N.IBaseNode,
-						   config?           : MCMFConfig ) 
+						   config?           : MCMFConfig )
   {
      this._config = config || this.prepareMCMFStandardConfig();
-  } 
+  }
 
 
   calculateCycle() {
@@ -66,6 +68,73 @@ class MCMFBoykov implements IMCMFBoykov {
 
     return result;
   }
+
+	tree(node: $N.IBaseNode) {
+		var tree: string = "";
+		if (node.getID() in this._state.treeS) {
+		    tree = "S";
+				return tree;
+		}
+		if (node.getID() in this._state.treeT) {
+		    tree = "T";
+				return tree;
+		}
+		return tree;
+	}
+
+	getPathToRoot(node: $N.IBaseNode) {
+		var path: Array<$N.IBaseNode>;
+		path.push(node);
+		while (this._state.parents[node.getID()]) {
+			node = this._state.parents[node.getID()];
+			path.push(this._state.parents[node.getID()]);
+		}
+		return path;
+	}
+
+	grow() {
+		// as long as there are active nodes
+		while (Object.keys(this._state.activeNodes).length) {
+			// take an active node
+			var activeNode: $N.IBaseNode = this._state.activeNodes[Object.keys(this._state.activeNodes)[0]];
+			// get all his neighbors
+			var neighbors: Array<$N.NeighborEntry> = activeNode.reachNodes();
+			// for all neighbors
+			for (let i = 0; i < neighbors.length; i++) {
+			    var neighborNode: $N.IBaseNode = neighbors[i].node;
+					if (this.tree(neighborNode) == "") {
+						// add neighbor to corresponding tree
+						(this.tree(activeNode) == "S") ? this._state.treeS[neighborNode.getID()] = neighborNode : this._state.treeT[neighborNode.getID()] = neighborNode;
+						// set active node as parent to neighbor node
+						this._state.parents[neighborNode.getID()] = activeNode;
+						// add neighbor to active node set
+						this._state.activeNodes[neighborNode.getID()] = neighborNode;
+					}
+					else if(this.tree(neighborNode) != this.tree(activeNode)) {
+						// constructing path
+						var path: Array<$N.IBaseNode>;
+
+						var nPath: Array<$N.IBaseNode> = this.getPathToRoot(neighborNode);
+						var aPath: Array<$N.IBaseNode> = this.getPathToRoot(activeNode);
+
+						var root_node_npath: $N.IBaseNode = nPath[nPath.length-1];
+						if (this.tree(root_node_npath) == "S") {
+						    nPath = nPath.reverse();
+								path = nPath.concat(aPath);
+						}
+						else {
+							aPath = aPath.reverse();
+							path = aPath.concat(nPath);
+						}
+						
+						this._state.path = path;
+						return this._state.path;
+					}
+			}
+			delete this._state.activeNodes[activeNode.getID()];
+		}
+		return []; //empty path
+	}
 
 
 
@@ -79,5 +148,5 @@ class MCMFBoykov implements IMCMFBoykov {
 
 
 export {
-  MCMFBoykov 
+  MCMFBoykov
 };
