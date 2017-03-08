9432a196bcbbe2c75db42b8c11927f2b8218987a
Bernd Malle <bernd.malle@gmail.com>
2017-03-08 19:39:53 +0100

    added default weight (=1) into PFS algorithm and performance tested closeness centrality with a social network of 1k nodes... 30sec...

--- test/centralities/ClosenessCentralityTests.ts
+++ test/centralities/ClosenessCentralityTests.ts
@@ -3,12 +3,17 @@
 import * as chai from 'chai';
 import * as $N from '../../src/core/Nodes';
 import * as $G from '../../src/core/Graph';
-import * as $I from '../../src/io/input/JSONInput';
+import * as $CSV from '../../src/io/input/CSVInput';
+import * as $JSON from '../../src/io/input/JSONInput';
 import * as $DC from '../../src/centralities/Closeness';
 
+const SN_GRAPH_NODES = 1034,
+      SN_GRAPH_EDGES = 53498 / 2; // edges are specified in directed fashion
 
-var expect = chai.expect,
-    json   : $I.IJSONInput = new $I.JSONInput(true, false, true),
+let expect = chai.expect,
+    csv : $CSV.ICSVInput = new $CSV.CSVInput(" ", false, false),
+    json   : $JSON.IJSONInput = new $JSON.JSONInput(true, false, true),
+    sn_graph_file = "./test/test_data/social_network_edges.csv",
     deg_cent_graph = "./test/test_data/search_graph_pfs_extended.json",
     graph : $G.IGraph = json.readFromJSONFile(deg_cent_graph);
 
@@ -33,6 +38,23 @@ describe("Closeness Centrality Tests", () => {
         let closeness_map = $DC.closenessCentrality( graph );
         expect( closeness_map ).to.deep.equal( expected_closeness_map );
     });
+
+
+    /**
+     * Performance measurement
+     * 
+     * TODO: Outsource to it's own performance test suite
+     */
+    it.skip('should run the closeness centrality on a real-sized social network', () => {
+        let sn_graph = csv.readFromEdgeListFile(sn_graph_file);
+        expect(sn_graph.nrNodes()).to.equal(SN_GRAPH_NODES);
+        expect(sn_graph.nrUndEdges()).to.equal(SN_GRAPH_EDGES);
+
+        // console.log(sn_graph.getRandomUndEdge().isWeighted());
+
+        let deg_dist = $DC.closenessCentrality( sn_graph );
+    });
+
     /*
      it('Single Closeness test on node A', () => {
      expect( graph.getNodeById("A").inDegree() ).to.equal( 2 );
