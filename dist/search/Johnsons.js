85728a6511e06a032b81d1b2351f5a5d7587c796
Bernd Malle <bernd.malle@gmail.com>
2017-11-29 16:28:00 +0100

    added my answers and suggestions

--- dist/search/Johnsons.js
+++ dist/search/Johnsons.js
@@ -1,39 +1,63 @@
 "use strict";
 var $N = require('../core/Nodes');
-var $E = require('../core/Edges');
-var $BF = require('../search/BellmanFord');
-var $D = require('../search/Dijkstra');
-var extraNode = new $N.BaseNode("extraNode");
-function Johnsons(graph, start, cycle) {
-    if (cycle === void 0) { cycle = false; }
-    addExtraNodeWithEdges(graph);
-    if (graph.hasNegativeCycles()) {
-        throw new Error('Can not continue: the graph has negative cycle');
+var $PFS = require('../../src/search/PFS');
+var BellmanFord_1 = require('../search/BellmanFord');
+var Dijkstra_1 = require('../search/Dijkstra');
+function Johnsons(graph, cycle) {
+    if (cycle === void 0) { cycle = true; }
+    var allNodes = graph.getNodes();
+    var nodeKeys = Object.keys(allNodes);
+    var extraNode = new $N.BaseNode("extraNode");
+    var graphForBF = graph.clone();
+    graphForBF.addNode(extraNode);
+    var count = 0;
+    for (var _i = 0, nodeKeys_1 = nodeKeys; _i < nodeKeys_1.length; _i++) {
+        var nodeId = nodeKeys_1[_i];
+        graphForBF.addEdgeByNodeIDs("temp" + count++, "extraNode", nodeId, { directed: true, weighted: true, weight: 0 });
+    }
+    for (var i = 0; i < nodeKeys.length; i++) {
+        var actualNode = allNodes[nodeKeys[i]];
+        graphForBF.addEdgeByID("temp" + i, extraNode, actualNode, { directed: true, weighted: true, weight: 0 });
+    }
+    if (BellmanFord_1.BellmanFordArray(graphForBF, extraNode, true)) {
+        throw new Error("The graph contains negative cycle(s), it makes no sense to continue");
     }
     else {
-        var resultBF = $BF.BellmanFordArray(graph, extraNode, true);
-        removeExtraNodeWithEdges(graph);
-        var allNodes = graph.getNodes();
-        var resultJohnson = void 0;
-        for (var count = 0; count < graph.nrNodes(); count++) {
-            var source = allNodes[count];
-            var actualResult = $D.Dijkstra(graph, source);
+        var newWeights = BellmanFord_1.BellmanFordDict(graphForBF, extraNode, false);
+        delete newWeights["extraNode"];
+        var RWGraph = graph.clone();
+        var edges = RWGraph.getDirEdgesArray().concat(RWGraph.getUndEdgesArray());
+        for (var edgeID in edges) {
+            var edge = edges[edgeID];
+            var a = edge.getNodes().a.getID();
+            var b = edge.getNodes().b.getID();
+            if (edge.isWeighted) {
+                var oldWeight = edge.getWeight();
+                var newWeight = oldWeight + newWeights[a] - newWeights[b];
+                edge.setWeight(newWeight);
+            }
+            else {
+                var edgeID_1 = edge.getID();
+                var dirNess = edge.isDirected();
+                RWGraph.deleteEdge(edge);
+                var oldWeight = $PFS.DEFAULT_WEIGHT;
+                var newWeight = oldWeight + newWeights[a] - newWeights[b];
+                RWGraph.addEdgeByNodeIDs(edgeID_1, a, b, { directed: dirNess, weighted: true, weight: newWeight });
+            }
         }
-        return resultJohnson;
+        var RWAllNodes = RWGraph.getNodes();
+        var RWNodeKeys = Object.keys(RWAllNodes);
+        for (var _a = 0, RWNodeKeys_1 = RWNodeKeys; _a < RWNodeKeys_1.length; _a++) {
+            var nodeID = RWNodeKeys_1[_a];
+            var resultD = Dijkstra_1.Dijkstra(RWGraph, RWAllNodes[nodeID]);
+            var targetKeys = Object.keys(resultD);
+            for (var targetNode in targetKeys) {
+                var entryForThisTarget = resultD[targetNode];
+                var distanceForThisTarget = entryForThisTarget.distance;
+                var parentForThisTarget = entryForThisTarget.parent;
+            }
+        }
     }
+    return {};
 }
 exports.Johnsons = Johnsons;
-function addExtraNodeWithEdges(graph) {
-    var allNodes = graph.getNodes();
-    graph.addNode(extraNode);
-    Object.keys(graph.getNodes());
-    for (var count = 0; count < graph.nrNodes(); count++) {
-        var target = allNodes[count];
-        var tempEdge = new $E.BaseEdge("temp", extraNode, target, { directed: true, weighted: true, weight: 0 });
-    }
-    return graph;
-}
-function removeExtraNodeWithEdges(graph) {
-    graph.deleteNode(extraNode);
-    return graph;
-}
--- src/search/Johnsons.ts
+++ src/search/Johnsons.ts
@@ -5,7 +5,7 @@ import * as $G from '../core/Graph';
 import * as $PFS from '../../src/search/PFS';
 import * as $BF from '../search/BellmanFord';
 import * as $D from '../search/Dijkstra';
-import { BellmanFordDict } from '../search/BellmanFord';
+import { BellmanFordDict, BellmanFordArray } from '../search/BellmanFord';
 import { Dijkstra } from '../search/Dijkstra';
 
 
@@ -17,7 +17,6 @@ function Johnsons(graph: $G.IGraph, cycle = true): { [id: string]: $PFS.PFS_Resu
 
   let nodeKeys = Object.keys(allNodes); 
   
-
   //the extra node we need to re-weigh the edges
   var extraNode: $N.IBaseNode = new $N.BaseNode("extraNode");
 
@@ -32,35 +31,64 @@ function Johnsons(graph: $G.IGraph, cycle = true): { [id: string]: $PFS.PFS_Resu
   //==================!
   //I made 2 for loops, I suppose both are good, but now for safety I left both to show you
   //please tell which is better (if any of them is good, but the compiler is not crying... so I hope they are OK)
+  
+  // @answer<Bernd>
+  // both will work, but it might be that the 2nd is faster than the first
+  // see this performance shootout:
+  // https://jsperf.com/performance-of-array-vs-object/3
+  // for now I would suggest leaving both in and commenting one out, so that we
+  // can performance test later...
+  // I am experimenting now with writing code in Rust and compiling it to WASM,
+  // if that works well we might re-implement compute-intensive methods in this way...
 
   //=================!
   //here I added a counter to the edge ID, so that each added edge has a different label
   //I think this is not important, because it is the ID which counts
   //if this is really so, I can remove the counter
+
+  // @answer<Bernd>
+  // Yes, it seems to me this is irrelevant, as we only need the distances <extraNode->node(v_i)>
   let count = 0;
   for (let nodeId of nodeKeys) {
     graphForBF.addEdgeByNodeIDs("temp" + count++, "extraNode", nodeId, { directed: true, weighted: true, weight: 0 });
   }
 
   //OR - alternative for loop, I suppose both are good, please correct if not
-    for (let i = 0; i < nodeKeys.length; i++) {
+  for (let i = 0; i < nodeKeys.length; i++) {
     let actualNode = allNodes[nodeKeys[i]];
     graphForBF.addEdgeByID("temp"+i, extraNode, actualNode, { directed: true, weighted: true, weight: 0 });
   }
 
   //now the Bellman-Ford, and re-weighting, if there are no negative edges
-  if (! <boolean>BellmanFordDict(graphForBF, extraNode, true)) {
+
+  // @correction<Bernd>
+  // if you only want to know about negative cycles, the array version is faster
+  // and the return value is 'true' if there is indeed a negative cycle
+  if (<boolean>BellmanFordArray(graphForBF, extraNode, true)) {
+  // if (! <boolean>BellmanFordDict(graphForBF, extraNode, true)) {
     throw new Error("The graph contains negative cycle(s), it makes no sense to continue");
     //=================!
     //do I need to put in some "break" command here?
     //I mean, if there is negative cycle, there is no sense to continue
     //or should I simply put the rest of the code into an "else" block, as I did now? (pls see where this coming "else" block ends)
+
+    // @answer<Bernd>
+    // nope, forget the else, if this one throws an error, the rest of the code won't execute anyways
   }
+
   else {
-    let newWeights: {} = <{}>BellmanFordDict(graphForBF, extraNode, true);
+    // @correction<Bernd>
+    // here you don't want to check for a cycle, right?
+    let newWeights: {} = <{}>BellmanFordDict(graphForBF, extraNode, false);
+    // let newWeights: {} = <{}>BellmanFordDict(graphForBF, extraNode, true);
 
     //deleting the distance 0 of the extraNode, it is no more needed
     delete newWeights["extraNode"];
+
+    // @suggestion<Bernd>
+    // don't you wanna delete the extraNode here again?
+    // graph.deleteNode(extraNode);
+
 
     //reminder: w(e)'=w(e)+dist(a)-dist(b), a and b the start and end nodes of the edge
     //RWGraph will be a clone of the original graph, then no node deletion needed
@@ -84,6 +112,11 @@ function Johnsons(graph: $G.IGraph, cycle = true): { [id: string]: $PFS.PFS_Resu
         //so if not weighted, I delete the edge, then create a new one
         //with the same ID and directedness, but weighted. The old weight is set artificially to 1. 
 
+        // @answer<Bernd>
+        // good thinking, this scenario shows you the complexities of a mixed mode graph
+        // this is why most other graph libraries make these artificial distinctions
+        // between graph, digraph and weighted (di)graph.. ;-))
+
         //collecting edgeID and directedness for later re-use
         let edgeID:string=edge.getID();
         let dirNess: boolean= edge.isDirected();
@@ -103,19 +136,34 @@ function Johnsons(graph: $G.IGraph, cycle = true): { [id: string]: $PFS.PFS_Resu
      //reminder: return type of PFS (last step of Dijkstras) is a dict
      //keys are IDs of the target nodes, values are PFS entries (distance, parent, count)
 
-    for (let nodeID of RWNodeKeys){
+    for (let nodeID of RWNodeKeys) {
       let resultD= Dijkstra(RWGraph, RWAllNodes[nodeID]);
       let targetKeys=Object.keys(resultD);
-        for (let targetNode in targetKeys){
-          let entryForThisTarget = resultD[targetNode];
-          let distanceForThisTarget = entryForThisTarget.distance;
-          let parentForThisTarget = entryForThisTarget.parent;
-        //and here I should build the final return structure once I figure out how that should look   
-         
-          
-        }
+
+      for (let targetNode in targetKeys){
+        let entryForThisTarget = resultD[targetNode];
+        let distanceForThisTarget = entryForThisTarget.distance;
+        let parentForThisTarget = entryForThisTarget.parent;
+      //and here I should build the final return structure once I figure out how that should look   
+        
+      // @answer<Bernd>
+      // exactly - and here I would have a look at Benedikt's code, or better:
+      // take a look at how the object wich is called in 'centralities/Betweenness.ts', 
+      // line 27 is constructed in 'search/FloydWarshall.ts' 
+      // in principle, it's this 'next' object you would have to construct and then
+      // return [dists, next]; -> as in 'search/FloydWarshall.ts' line 78
+      // so your job is to somehow merge the n dijkra return structures into those
+      // dists & next structures ;)
+      // then in 'centralities/Betweenness.ts', we could simply do a check on the
+      // number of nodes and edges in the graph, and if it is dense, we keep the
+      // call to FloydWarshall, but if it is sparse, we call Johnson's => easy!          
+      }
+      
     }
-    //return return structure here
+    //return structure here
+
+
+
 
   }
   //this part is under construction
