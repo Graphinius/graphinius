2a9303a8ba932ef957ee17ffde13090d8b6435a9
Bernd Malle <bernd.malle@gmail.com>
2019-08-05 20:20:38 +0200

    completed NO edge dupes test cases

--- test/io/common/Dupes.test.ts
+++ test/io/common/Dupes.test.ts
@@ -70,7 +70,88 @@ describe('Edge DUPE tests', function () {
 	});
 
 
-	it('set of potential duplicate should be empty', () => {
+	/**
+	 * Empty set -> NO potential dupes
+	 *
+	 * @description occurs in the following cases:
+	 * 							-) endpoints are completely different
+	 * 								i. directed vs. undirected (2)
+	 * 						  -) only one endpoint is the same
+	 *								i. a vs b (2)
+	 * 						  	ii. directed vs. undirected (*2 = 4)
+	 * 						  -) both endpoints are the same, but
+	 * 						  	i.  direction type is different
+	 * 						   		- a->b, directed vs. UN (1)
+	 * 						   	  - b->a, directed vs. UN (1)
+	 * 						  	ii. direction is different
+	 * 						      - a->b vs. b->a (1)
+	 * 						      - b->a vs. a->b (1)
+	 */
+
+	it('endpoints are completely different (same direction type)', () => {
+		e_2 = graph.addEdgeByNodeIDs(v4(), c.id, d.id, {directed: true});
+		const newEdgeInfo: PotentialEdgeInfo = {
+			a, b,	dir: true,	weighted: false, typed: false
+		};
+		expect(edc.potentialEndpoints(newEdgeInfo)).toEqual(new Set());
+	});
+
+
+	/**
+	 * @todo necessary?
+	 */
+	it('endpoints are completely different (different direction type)', () => {
+		e_2 = graph.addEdgeByNodeIDs(v4(), c.id, d.id);
+		const newEdgeInfo: PotentialEdgeInfo = {
+			a, b,	dir: true,	weighted: false, typed: false
+		};
+		expect(edc.potentialEndpoints(newEdgeInfo)).toEqual(new Set());
+	});
+
+
+	it('only one endpoint is the same (a, same direction type)', () => {
+		e_2 = graph.addEdgeByNodeIDs(v4(), a.id, d.id, {directed: true});
+		const newEdgeInfo: PotentialEdgeInfo = {
+			a, b,	dir: true,	weighted: false, typed: false
+		};
+		expect(edc.potentialEndpoints(newEdgeInfo)).toEqual(new Set());
+	});
+
+
+	/**
+	 * @todo necessary?
+	 */
+	it('only one endpoint is the same (a, different direction type)', () => {
+		e_2 = graph.addEdgeByNodeIDs(v4(), a.id, d.id);
+		const newEdgeInfo: PotentialEdgeInfo = {
+			a, b,	dir: true,	weighted: false, typed: false
+		};
+		expect(edc.potentialEndpoints(newEdgeInfo)).toEqual(new Set());
+	});
+
+
+	it('only one endpoint is the same (b, same direction type)', () => {
+		e_2 = graph.addEdgeByNodeIDs(v4(), c.id, a.id, {directed: true});
+		const newEdgeInfo: PotentialEdgeInfo = {
+			a, b,	dir: true,	weighted: false, typed: false
+		};
+		expect(edc.potentialEndpoints(newEdgeInfo)).toEqual(new Set());
+	});
+
+
+	/**
+	 * @todo necessary?
+	 */
+	it('only one endpoint is the same (b, different direction type)', () => {
+		e_2 = graph.addEdgeByNodeIDs(v4(), c.id, a.id);
+		const newEdgeInfo: PotentialEdgeInfo = {
+			a, b,	dir: true,	weighted: false, typed: false
+		};
+		expect(edc.potentialEndpoints(newEdgeInfo)).toEqual(new Set());
+	});
+
+
+	it('same endpoints, but undirected & directed edges can never be dupes ', () => {
 		e_2 = graph.addEdgeByNodeIDs(v4(), a.id, b.id, {directed: true});
 		const newEdgeInfo: PotentialEdgeInfo = {
 			a, b,	dir: false,	weighted: false, typed: false
@@ -79,10 +160,7 @@ describe('Edge DUPE tests', function () {
 	});
 
 
-	/**
-	 * Reversed direction
-	 */
-	it('set of potential duplicate should be empty', () => {
+	it('same endpoints, undirected & directed edges can never be dupes (reversed direction)', () => {
 		e_1 = graph.addEdgeByNodeIDs(v4(), b.id, a.id, {directed: true});
 		const newEdgeInfo: PotentialEdgeInfo = {
 			a, b,	dir: false,	weighted: false, typed: false
@@ -91,34 +169,83 @@ describe('Edge DUPE tests', function () {
 	});
 
 
-	it('should find e_1 edge as potential set of edge dupes', () => {
-		e_1 = graph.addEdgeByNodeIDs(v4(), a.id, b.id);
-		e_2 = graph.addEdgeByNodeIDs(v4(), a.id, b.id, {directed: true});
-		const newEdgeInfo: PotentialEdgeInfo = {
-			a, b,	dir: false,	weighted: false, typed: false
-		};
-		expect(edc.potentialEndpoints(newEdgeInfo)).toEqual(new Set([e_1]));
-	});
-
-
-	it('should find both edges edge as potential set of edge dupes', () => {
-		e_1 = graph.addEdgeByNodeIDs(v4(), a.id, b.id);
-		e_2 = graph.addEdgeByNodeIDs(v4(), b.id, a.id);
-		const newEdgeInfo: PotentialEdgeInfo = {
-			a, b,	dir: false,	weighted: false, typed: false
-		};
-		expect(edc.potentialEndpoints(newEdgeInfo)).toEqual(new Set([e_1, e_2]));
-	});
-
-
-	it('should find both edges edge as potential set of edge dupes', () => {
-		e_1 = graph.addEdgeByNodeIDs(v4(), a.id, b.id, {directed: true});
-		e_2 = graph.addEdgeByNodeIDs(v4(), a.id, b.id, {directed: true});
+	it('same endpoints, both directed, but reversed direction', () => {
+		e_1 = graph.addEdgeByNodeIDs(v4(), b.id, a.id, {directed: true});
 		const newEdgeInfo: PotentialEdgeInfo = {
 			a, b,	dir: true,	weighted: false, typed: false
 		};
-		expect(edc.potentialEndpoints(newEdgeInfo)).toEqual(new Set([e_1, e_2]));
+		expect(edc.potentialEndpoints(newEdgeInfo)).toEqual(new Set());
 	});
+
+
+	it('same endpoints, both directed, but reversed direction', () => {
+		e_1 = graph.addEdgeByNodeIDs(v4(), a.id, b.id, {directed: true});
+		const newEdgeInfo: PotentialEdgeInfo = {
+			a:b, b:a,	dir: true,	weighted: false, typed: false
+		};
+		expect(edc.potentialEndpoints(newEdgeInfo)).toEqual(new Set());
+	});
+
+
+
+	/**
+	 * Same endpoints - are edges (pairwise) both typed and weighted?
+	 *
+	 * @description We consider both directed & undirected combinations here,
+	 * 							although it's probably not necessary
+	 * 						  -) weighted vs. unweighted
+	 * 						  -) typed vs. untyped
+	 * @todo adapt `no dupe scenarios` to ever more exact specification as you go...
+	 */
+
+	// it('should find e_1 as potential dupe (both undirected)', () => {
+	// 	e_1 = graph.addEdgeByNodeIDs(v4(), a.id, b.id, {directed: true, weighted: true});
+	// 	const newEdgeInfo: PotentialEdgeInfo = {
+	// 		a, b,	dir: true,	weighted: false, typed: false
+	// 	};
+	// 	expect(edc.potentialEndpoints(newEdgeInfo)).toEqual(new Set([e_1]));
+	// });
+	//
+	//
+	//
+	// it('should find both edges as potential dupe (both undirected)', () => {
+	// 	e_1 = graph.addEdgeByNodeIDs(v4(), a.id, b.id);
+	// 	e_2 = graph.addEdgeByNodeIDs(v4(), a.id, b.id);
+	// 	const newEdgeInfo: PotentialEdgeInfo = {
+	// 		a, b,	dir: false,	weighted: false, typed: false
+	// 	};
+	// 	expect(edc.potentialEndpoints(newEdgeInfo)).toEqual(new Set([e_1, e_2]));
+	// });
+	//
+	//
+	// it('should find both edges as potential dupes', () => {
+	// 	e_1 = graph.addEdgeByNodeIDs(v4(), a.id, b.id);
+	// 	e_2 = graph.addEdgeByNodeIDs(v4(), b.id, a.id);
+	// 	const newEdgeInfo: PotentialEdgeInfo = {
+	// 		a, b,	dir: false,	weighted: false, typed: false
+	// 	};
+	// 	expect(edc.potentialEndpoints(newEdgeInfo)).toEqual(new Set([e_1, e_2]));
+	// });
+	//
+	//
+	// it('should find both edges as potential dupes', () => {
+	// 	e_1 = graph.addEdgeByNodeIDs(v4(), a.id, b.id, {directed: true});
+	// 	e_2 = graph.addEdgeByNodeIDs(v4(), a.id, b.id, {directed: true});
+	// 	const newEdgeInfo: PotentialEdgeInfo = {
+	// 		a, b,	dir: true,	weighted: false, typed: false
+	// 	};
+	// 	expect(edc.potentialEndpoints(newEdgeInfo)).toEqual(new Set([e_1, e_2]));
+	// });
+	//
+	//
+	// it('should find e_2 edges edge as potential dupe (reverse direction)', () => {
+	// 	e_1 = graph.addEdgeByNodeIDs(v4(), b.id, a.id, {directed: true});
+	// 	e_2 = graph.addEdgeByNodeIDs(v4(), a.id, b.id, {directed: true});
+	// 	const newEdgeInfo: PotentialEdgeInfo = {
+	// 		a, b,	dir: true,	weighted: false, typed: false
+	// 	};
+	// 	expect(edc.potentialEndpoints(newEdgeInfo)).toEqual(new Set([e_2]));
+	// });
 
 
 });
